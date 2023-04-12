dee2d034f55fcf7ec7845dccccdc99ea0d3ab06b
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-12 14:40:19 -0600

    Removed unused font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import { signIn, signOut, useSession } from 'next-auth/react';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const { data: session, status } = useSession();
