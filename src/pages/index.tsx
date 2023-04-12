9813d5e92ff43bfd9daf88a2f82e9bc61b90efcc
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-12 14:39:17 -0600

    Added basic google OAuth 2.0 login with next-auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <div>Home Page</div>;
+  const { data: session, status } = useSession();
+  const userEmail = session?.user?.email;
+
+  if (status === 'loading') {
+    return <p>Hang on there...</p>;
+  }
+
+  if (status === 'authenticated') {
+    return (
+      <>
+        <p>Signed in as {userEmail}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+        <img src="/images/generated-mecha-thumbnail.png" />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <p>Not signed in.</p>
+      <button onClick={() => signIn('google')}>Sign in</button>
+    </>
+  );
 }
